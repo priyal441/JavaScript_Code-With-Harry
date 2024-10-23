649307605e60148704a2110d626ca4b32d7f5f13
priyal gupta <122721018+priyal441@users.noreply.github.com>
2024-10-23 07:24:32 +0530

    Add files via upload

---
+++ Praticeset1.js
@@ -0,0 +1,32 @@
+//problem 1
+let a="harry";
+let b=5;
+console.log(a+b);
+
+//problem 2
+console.log(typeof (a+b));
+//problem 3
+const tree={
+  "brown":true,
+  "red":false,
+  "green":undefined,
+  "section":1
+}
+//tree="brown"
+//this problem raise error
+
+//problem 4
+tree['friend']="sherya"
+tree['name']="Lovish"
+console.log(tree)
+//tree={}//this problem raise error
+
+//problem 5
+const dict={
+  drizzling:"little rain",
+  candid:"frank",
+  candidly:"franly",
+  understand:"figureout"
+}
+console.log(dict.candid)
+console.log(dict['understand'])
